2c9a772c92d415d68cd7b4489a6e7280af03937d
ben <benj.groeneveld@gmail.com>
2017-07-24 18:50:10 +0200

    locktask refactor

--- src/providers/lock-task.ts
+++ src/providers/lock-task.ts
@@ -6,39 +6,38 @@ declare var window: any;
 @Injectable()
 export class LockTask {
 
-  private _locktask: any = null;
-
   constructor(private plt: Platform) {
-    plt.ready().then(() => {
-      if (plt.is('cordova') && plt.is('android'))
-        this._locktask = window.plugins.locktask;
-    })
+
   }
 
-  private _noCordova(): Promise<any> {
-    return Promise.reject('No Cordova or Android');
+  get locktaskPromise(): Promise<any> {
+    return this.plt.ready().then(() => {
+      if (this.plt.is('cordova') && this.plt.is('android'))
+        return window.plugins.locktask;
+      throw 'No Cordova or Android';
+    });
   }
 
   start(className?: string): Promise<any> {
-    if (this._locktask === null)
-      return this._noCordova();
-    return new Promise((resolve, reject) => {
-      this._locktask.startLockTask(
-        (success) => resolve(success),
-        (error) => reject(error),
-        className
-      );
-    })
+    return this.locktaskPromise.then((locktask) => {
+      return new Promise((resolve, reject) => {
+        locktask.startLockTask(
+          (success) => resolve(success),
+          (error) => reject(error),
+          className
+        );
+      })
+    });
   }
 
   stop(): Promise<any> {
-    if (this._locktask === null)
-      return this._noCordova();
-    return new Promise((resolve, reject) => {
-      this._locktask.stopLockTask(
-        (success) => resolve(success),
-        (error) => reject(error)
-      );
-    })
+    return this.locktaskPromise.then((locktask) => {
+      return new Promise((resolve, reject) => {
+        locktask.stopLockTask(
+          (success) => resolve(success),
+          (error) => reject(error)
+        );
+      })
+    });
   }
 }
